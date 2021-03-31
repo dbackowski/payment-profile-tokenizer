5fefdd0a872fe4b24d33f10c05acc0836ffbc8f3
Damian Baćkowski <damianbackowski@gmail.com>
2021-03-31 20:22:03 +0200

    Add more formatters

--- src/shared/inputFormatter.js
+++ src/shared/inputFormatter.js
@@ -1,6 +1,9 @@
 export default class InputFormatter {
   static AVAILABLE_FORMATS = {
     CREDIT_CARD: InputFormatter.creditCardFormat,
+    MONTH: InputFormatter.monthFormat,
+    YEAR: InputFormatter.yearFormat,
+    CVC: InputFormatter.cvcFormat,
   };
 
   static format(type, value) {
@@ -12,6 +15,20 @@ export default class InputFormatter {
   }
 
   static creditCardFormat(value) {
-    return value.replace(new RegExp(/[^\d]/, 'ig'), '');
+    return value.replace(new RegExp(/[^\d]/, 'ig'), '')
+      .slice(0, 16)
+      .split(/([0-9]{4})/g);
+  }
+
+  static monthFormat(value) {
+    return value.replace(new RegExp(/[^\d]/, 'ig'), '').slice(0, 2);
+  }
+
+  static yearFormat(value) {
+    return value.replace(new RegExp(/[^\d]/, 'ig'), '').slice(0, 4);
+  }
+
+  static cvcFormat(value) {
+    return value.replace(new RegExp(/[^\d]/, 'ig'), '').slice(0, 4);
   }
 }
