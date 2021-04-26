d2fec764c0375a1824389e0a8c8f09fafca67e5e
Damian Baćkowski <damianbackowski@gmail.com>
2021-04-26 09:39:15 +0200

    Add TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -12,13 +12,15 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
+      { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      { test: /\.js$/, loader: 'source-map-loader' },
     ],
   },
   plugins: [
